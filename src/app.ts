907af98d3c082ff2acdb08d0caf4c4e6075647b2
emreturgutce <emrebursalee@gmail.com>
2020-07-31 00:05:03 +0300

    validator jwt downloaded

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,16 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import 'colors';
 import './config/database';
+import RootRoute from './routes/';
+import UserRoute from './routes/user';
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+app.use('/', RootRoute);
+app.use('/users', UserRoute);
 
 export default app;
