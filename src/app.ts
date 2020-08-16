4f5a68f818480e64cc2ccab4dab2c329ecefa1a5
emreturgutce <emrebursalee@gmail.com>
2020-08-16 16:07:10 +0300

    error handler and not found mw and passport authentication mw for user route

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,21 @@ import 'colors';
 import './config/database';
 import RootRoute from './routes/';
 import UserRoute from './routes/user';
+import passport from './config/passport';
+import notFound from './middleware/notFound';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+
 app.use('/', RootRoute);
-app.use('/users', UserRoute);
+app.use('/user', passport.authenticate('jwt', { session: false }), UserRoute);
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
